e336b2af8454f53a150e513a75a9d85d00c6360f
lnaragun <lnaragun@DIN76000819.corp.capgemini.com>
2017-11-29 10:14:09 +0530

    commit changes

---
+++ GItDEmo11292017/src/main/java/com/capgemini/GItDEmo11292017/App.java
@@ -0,0 +1,17 @@
+package com.capgemini.GItDEmo11292017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       greetings();
+    }
+    public static void greetings()
+    {
+    	System.out.println("Welcome Good morning");
+    }
+}
